2441d295bafb2b308b2d4c8a438c55e9efc48753
EshwarNaikG <eshwarnaik79@gmail.com>
2025-12-29 20:03:01 +0530

    initial coment

---
+++ script.js
@@ -0,0 +1,8 @@
+function orderNow() {
+    alert("Thank you for choosing Delicious Restaurant! 🍽️");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Message sent successfully! 📩");
+}
